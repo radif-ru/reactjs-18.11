f9fbe4596a402e90a35fbb48cf3e319cd7dd6ce4
Radif <i@radif.ru>
2021-05-11 10:35:11 +0300

    lesson01

--- Radif/src/index.jsx
+++ Radif/src/index.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import {User} from "./user";
+
+const user = new User('Oleg', 'Petrovich')
 const messages = ['Привет, друг', 'How are you doing?'];
 
 const Message = (props) => <div>{props.text}</div>;
 const MessageList = (props) => {
+    // const el = props.messages.map((message) => <Message text={message} />);
+    // console.log(el);
     return props.messages.map((message) => <Message text={message} />)
 }
+
+ReactDom.render(
+    <Message text={user.sayHi()} />,
+    document.getElementById('user')
+);
 
 ReactDom.render(
     <MessageList messages={messages} />,
